298863246cb85cbe2302dacdcc7f2a9c3fe269ff
John <jdavidson091@gmail.com>
2014-08-23 09:15:10 -0500

    First Commit

---
+++ desktop/src/com/connectedworld/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.connectedworld.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.connectedworld.game.ConnectedWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ConnectedWorld(), config);
+	}
+}
